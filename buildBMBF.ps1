b15a143a431f8ad81a1daaf5ea68493562fa9063
ivananaya12 <80081090+ivananaya12@users.noreply.github.com>
2021-03-10 01:59:39 -0600

    Create buildBMBF.ps1

---
+++ buildBMBF.ps1
@@ -0,0 +1,7 @@
+
+# Builds a .zip file for loading with BMBF
+& $PSScriptRoot/build.ps1
+
+if ($?) {
+    Compress-Archive -Path "./libs/arm64-v8a/lib#{id}.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_10.so", "./bmbfmod.json" -DestinationPath "./#{id}_v0.1.0.zip" -Update
+}
